1e446f4561017332f3e5bc225eef4038004949e6
Pandademic <atharv.gupte510@gmail.com>
2021-12-01 17:35:25 -0500

    logic for Y/n answers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+use std::process;
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to the guessing game! \n Guess a number from 1 to 1000. \n ready to begin?Y/n  ");
+    let mut reply = String::new();
+    io::stdin().read_line(&mut reply).expect("Error: Failed to read line !");
+    if reply == "Y"{
+        //continue
+        println!("Continuning...");
+    }else{
+      process::exit(1);  
+    }
 }
