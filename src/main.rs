f01f3809c74215293944ff6b41b411d5bdc4e3e9
Pandademic <atharv.gupte510@gmail.com>
2021-12-02 17:06:27 -0500

    add bonnie & update main file

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
-use std::io;
 use std::process;
+use std::io;
 fn main() {
     println!("Welcome to the guessing game! \n Guess a number from 1 to 1000. \n ready to begin?Y/n  ");
     let mut reply = String::new();
     io::stdin().read_line(&mut reply).expect("Error: Failed to read line !");
+    println!("Your response was : {}",reply );
     if reply == "Y"{
-        //continue
-        println!("Continuning...");
-    }else{
-      process::exit(1);  
+        println!("Okay then");
+        let mut isgamerunning = true;
+        while isgamerunning == true {
+            println!("game is running");
+        }
+    }else if reply == "n"{
+        println!("Exiting");
+        process::exit(1);
     }
 }
